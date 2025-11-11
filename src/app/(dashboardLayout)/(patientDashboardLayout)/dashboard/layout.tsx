ab09b6e2ff75a35fddf2c249c1d5f6b93d87fbb6
istiak19 <istiak15-14128@diu.edu.bd>
2025-11-11 15:11:48 +0600

    page created

---
+++ src/app/(dashboardLayout)/(patientDashboardLayout)/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+const PatientDashboardLayout = ({ children }: { children: ReactNode }) => {
+    return <div>{children}</div>;
+};
+
+export default PatientDashboardLayout;
