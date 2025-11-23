0dc7621b9ec0c4687bdfe932dbfca42d584c6c8b
istiak19 <istiak15-14128@diu.edu.bd>
2025-11-23 11:30:21 +0600

    Schedules Management

--- src/lib/server-fetch.ts
+++ src/lib/server-fetch.ts
@@ -1,11 +1,16 @@
+import { getNewAccessToken } from "@/services/auth/getNewAccessToken";
 import { getCookies } from "@/utility/tokenHandlers";
 
 const BACKEND_API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1";
 
 const serverFetchHelper = async (endpoint: string, options: RequestInit): Promise<Response> => {
     const { headers, ...restOptions } = options;
-
     const accessToken = await getCookies("accessToken");
+
+    //to stop recursion loop
+    if (endpoint !== "/auth/refresh-token") {
+        await getNewAccessToken();
+    };
 
     const response = await fetch(`${BACKEND_API_URL}${endpoint}`, {
         headers: {
